7033de08c8c6e1e2d26a538f25248b2f2e337a39
Twisty Skrill <egor.kobzev2018@yandex.ru>
2025-03-13 17:34:19 +0300

    base js

--- scripts/index.js
+++ scripts/index.js
@@ -6,4 +6,37 @@
 
 // @todo: Функция удаления карточки
 
+
 // @todo: Вывести карточки на страницу
+function createCard(cardData, cardRemove) {
+    const cardTemplate = document.querySelector('#card-template').content;
+    const card = cardTemplate.querySelector('.places__item').cloneNode(true);
+    const deleteButton = card.querySelector('.card__delete-button');
+    const cardImage = card.querySelector('.card__image');
+    const cardTitle = card.querySelector('.card__title');
+    
+    cardImage.src = cardData.link;
+    cardTitle.textContent = cardData.name;
+
+    deleteButton.addEventListener('click', () => {
+        cardRemove(card);
+    });
+
+    return card; 
+};
+
+function cardRemove(card) {
+    const deleteButton = card.querySelector('.card__delete-button');
+    const cardItem = deleteButton.closest('.places__item');
+    cardItem.remove();
+}
+
+const cardsContainer = document.querySelector('.places__list');
+for (let i = 0; i < initialCards.length; i++) {
+    const cardElement = createCard(initialCards[i], cardRemove);
+    cardsContainer.append(cardElement); 
+};
+
+
+
+
